0fb6df510cfadbcbfc51ad3fcb66ebf46a48df6b
suraj-chhabra-3 <surajchhabra3333@gmail.com>
2025-08-03 22:48:06 +0530

    deploy

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB();
 await connectCloudinary();
 
 // allow origins to access our back-end - cors
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173', 'https://gro-kart-three.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 
